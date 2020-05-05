23dc5f0aeb4592c01a44ba4d6622835d7b3d03ee
Tommy Parnell <tparnell@cargurus.com>
2020-05-05 15:34:56 -0400

    [publish binary] (#14)

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 COMMIT_MESSAGE=$(git log --format=%B --no-merges -n 1 | tr -d '\n')
 if [[ ${COMMIT_MESSAGE} =~ "[publish binary]" ]]
     then
-        yarn install
+        yarn
         yarn upload-binary
-        yarn publish
+        npm publish
     else
         echo "skipping publish";
         exit 0;
