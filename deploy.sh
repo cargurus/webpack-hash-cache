e03ac9fda078eba8b849a3e47434aee5c113d6ad
tparnell <tparnell@cargurus.com>
2020-05-07 12:07:48 -0400

    [publish binary]

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,10 @@ COMMIT_MESSAGE=$(git log --format=%B --no-merges -n 1 | tr -d '\n')
 if [[ ${COMMIT_MESSAGE} =~ "[publish binary]" ]]
     then
         yarn
+        echo -e "\n_authToken=${NPM_TOKEN}" >> .npmrc
         yarn upload-binary
         yarn publish
+        git checkout .npmrc
     else
         echo "skipping publish";
         exit 0;
