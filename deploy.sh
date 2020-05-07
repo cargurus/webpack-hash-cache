bead3fdd5cd519b306b13b086a33ab8c8cfecf19
colinfruit <17092461+colinfruit@users.noreply.github.com>
2020-05-07 19:04:29 -0400

    travis test

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 COMMIT_MESSAGE=$(git log --format=%B --no-merges -n 1 | tr -d '\n')
 if [[ ${COMMIT_MESSAGE} =~ "[publish binary]" ]]
     then
+        yarn
         echo -e "\n_authToken=${NPM_TOKEN}" >> .npmrc
         yarn upload-binary
         yarn publish
