dd9f710310ec281e57ca014211c16e895a5ea4c0
colinfruit <17092461+colinfruit@users.noreply.github.com>
2022-01-11 07:54:51 -0500

    add deploy job

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 COMMIT_MESSAGE=$(git log --format=%B --no-merges -n 1 | tr -d '\n')
 if [[ ${COMMIT_MESSAGE} =~ "[publish binary]" ]]
     then
-        yarn
+        npm
         echo -e "\n_authToken=${NPM_TOKEN}" >> .npmrc
-        yarn upload-binary
-        yarn clean
+        npm upload-binary
+        npm clean
 	      npm publish
         git checkout .npmrc
     else
