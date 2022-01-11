d49d23ad302450e328827d8e9b71dbb56297b19d
Colin Fruit <17092461+colinfruit@users.noreply.github.com>
2022-01-11 08:49:30 -0500

    Add deploy job (#42)
    * add cargo.lock
    
    * add deploy job
    
    * add NPM secret use
    
    * run deploy job on PRs for testing
    
    * add runs-on to deploy job
    
    * add os matrix
    
    * use deploy commands rather than deploy script
    
    * test
    
    * npm run upload-binary
    
    * test with yarn
    
    * add gh token
    
    * remove test commands from deploy job
    
    * test with alpha release
    
    * use publish action
    
    * yml syntax

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 COMMIT_MESSAGE=$(git log --format=%B --no-merges -n 1 | tr -d '\n')
 if [[ ${COMMIT_MESSAGE} =~ "[publish binary]" ]]
     then
-        yarn
-        echo -e "\n_authToken=${NPM_TOKEN}" >> .npmrc
-        yarn upload-binary
-        yarn clean
+        npm
+        echo -e "\n_authToken=${secrets.NPM_TOKEN}" >> .npmrc
+        npm upload-binary
+        npm clean
 	      npm publish
         git checkout .npmrc
     else
