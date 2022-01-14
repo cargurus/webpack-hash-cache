1de27f8e645388f4fe67bc9d3dc709bec288ae42
colinfruit <17092461+colinfruit@users.noreply.github.com>
2022-01-14 08:03:19 -0500

    migrate to napi

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
-#![allow(unused_must_use)]
-#![warn(clippy::all)]
+#![deny(clippy::all)]
 
-use neon::prelude::*;
+#[macro_use]
+extern crate napi_derive;
 
-use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
 use std::fs;
 use std::io;
@@ -14,10 +13,10 @@ use cache::{CachedEntry, CachedFile};
 
 mod cache;
 
-#[derive(Serialize, Deserialize)]
-struct Entries {
-    name: String,
-    files: HashSet<String>
+#[napi(object)]
+pub struct Entries {
+    pub name: String,
+    pub files: Vec<String>
 }
 
 fn walk_dir(cache_dir_path: &Path) -> io::Result<(HashSet<String>, HashSet<String>)> {
@@ -46,9 +45,10 @@ fn walk_dir(cache_dir_path: &Path) -> io::Result<(HashSet<String>, HashSet<Strin
     Ok((changed_entries, entries))
 }
 
-fn get_unchanged_entries(mut cx: FunctionContext) -> JsResult<JsValue> {
+#[napi]
+fn get_unchanged_entries(cache_dir: String) -> Result<Vec<String>, napi::Error> {
     // parse JS args
-    let cache_dir: String = cx.argument::<JsString>(0)?.value(&mut cx);
+    // let cache_dir: String = cx.argument::<JsString>(0)?.value(&mut cx);
     let cache_dir_path = Path::new(&cache_dir);
     // track entries and changed entries, and return the difference.
 
@@ -60,19 +60,14 @@ fn get_unchanged_entries(mut cx: FunctionContext) -> JsResult<JsValue> {
     let unchanged_entries: Vec<String> =
         entries.difference(&changed_entries).cloned().collect();
 
-    Ok(neon_serde3::to_value(&mut cx, &unchanged_entries).unwrap())
+    // Ok(neon_serde3::to_value(&mut cx, &unchanged_entries).unwrap())
+    // TODO: return vec of unchanged_entries
+    Ok(unchanged_entries)
 }
 
 // so JS can call it asynchronously
-fn cache_entries(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    let cache_dir = cx.argument::<JsString>(0)?.value(&mut cx);
-
-    let js_entries: Handle<JsValue> = cx.argument(1)?;
-
-    let entries: Vec<Entries> = neon_serde3::from_value(&mut cx, js_entries).unwrap();
-
-    let _f = cx.argument::<JsFunction>(2)?;
-
+#[napi]
+fn cache_entries(cache_dir: String, entries: Vec<Entries>) -> Result<(), napi::Error> {
     let cached_entries: Vec<CachedEntry> = entries
         .par_iter()
         .map(|entry| {
@@ -95,11 +90,5 @@ fn cache_entries(mut cx: FunctionContext) -> JsResult<JsUndefined> {
             .write(&cache_dir)
             .unwrap_or_else(|_| panic!("Error writing cache file for: {}", cached_entry.name));
     }
-    Ok(cx.undefined())
-}
-
-register_module!(mut m, {
-    m.export_function("getUnchangedEntries", get_unchanged_entries);
-    m.export_function("cacheEntries", cache_entries);
     Ok(())
-});
+}
